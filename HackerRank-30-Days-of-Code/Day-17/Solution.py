7965c18ec6d26e8818c534df101f1afaa3a762e9
Md. Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2020-08-22 23:34:28 +0600

    added

--- HackerRank-30-Days-of-Code/Day-17/Solution.py
+++
@@ -1,9 +0,0 @@
-S = input().strip()
-
-
-try:
-    conInt = int(S)
-    print(conInt)
-
-except ValueError:
-    print('Bad String')
---
+++ HackerRank-30-Days-of-Code/Day-20 Sorting/Solution.py
@@ -0,0 +1,18 @@
+import sys
+
+
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+numberOfSwaps = 0
+for i in range(0,n):
+    for j in range(0, n-1):
+        if (a[j] > a[j + 1]):
+            temp=a[j]
+            a[j] = a[j+1]
+            a[j+1] = temp
+            numberOfSwaps += 1
+    if (numberOfSwaps == 0):
+        break
+print( "Array is sorted in " + str(numberOfSwaps) + " swaps." )
+print( "First Element: " + str(a[0]) )
+print( "Last Element: " +  str(a[n-1]) )
