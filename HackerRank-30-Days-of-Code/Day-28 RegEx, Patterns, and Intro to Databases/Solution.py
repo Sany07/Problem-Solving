318cfcb66865866adc76134fa930b52c9b1ab32d
Md. Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2020-08-24 14:34:20 +0600

    solve added

---
+++ HackerRank-30-Days-of-Code/Day-28 RegEx, Patterns, and Intro to Databases/Solution.py
@@ -0,0 +1,16 @@
+import sys
+import re
+
+
+N = int(input().strip())
+names = []
+for _ in range(N):
+    firstName,emailID = input().strip().split(' ')
+    firstName,emailID = [str(firstName),str(emailID)]
+    match = re.search(r'[\w\.-]+@gmail.com', emailID)
+
+    if match:
+        names.append(firstName)
+names.sort()
+for name in names:
+    print( name )
